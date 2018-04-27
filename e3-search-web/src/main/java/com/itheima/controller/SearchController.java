fe8550cb96dbae318555efeb7fc9c0083bd8f31e
liufei <2317741837@qq.com>
2018-04-28 00:23:28 +0800

    增加了e3-search

---
+++ e3-search-web/src/main/java/com/itheima/controller/SearchController.java
@@ -0,0 +1,12 @@
+package com.itheima.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class SearchController {
+    @RequestMapping("search")
+    public String index(String keyword){
+        return "search";
+    }
+}
